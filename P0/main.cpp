dfaf1d1471fe356b9215fe0f118ad7ac1a08ba95
ericgoodwin <edgbpc@mail.umsl.edu>
2019-02-03 14:09:15 -0600

    Initial Commit

---
+++ P0/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  P0
+//
+//  Created by Eric Goodwin on 2/3/19.
+//  Copyright © 2019 umsl. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
